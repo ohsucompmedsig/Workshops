7d1ee8222c61f1ef69e042148ed1ab55e27806c7
Eric Cramer <ericcramer@Erics-MacBook-Air.local>
2022-02-13 00:32:55 -0800

    adding SBM 2022 workshop

--- workshop-20211213/R-workshop-20211213.R
+++ workshop-20211213/R-workshop-20211213.R
@@ -1,3 +1,4 @@
+setwd("/Users/ericcramer/OneDrive/Documents/coding-projects/ohsu-compsig/Workshops/workshop-20211213")
 ### OHSU Computation in Medicine SIG
 # 
 # BLHD Enrichment: Getting started with R for medical projects
@@ -24,50 +25,91 @@
 ### Input dataset, split in groups, perform T test - Sid
 
 # load the data set into a variable called "covidData"
-
+covidData <- read.csv("./countyCovidData.csv")
 
 # Check out some features of your data
+dim(covidData)
+nrow(covidData)
+ncol(covidData)
+head(covidData)
+tail(covidData)
+View(covidData)
 
 # Simple subsetting of data
-
+covidData[1,]
+covidData[1:5, ]
+covidData[, 1:5]
+covidData[1:5, 1:5]
 
 # Check features of read.csv()
 ?read.csv
-
+covidData <- covidData[, -1]
 
 # National level data
-
+natData <- read.csv("./nationalLevelData.csv")
+head(natData)
+natData <- natData[,-1]
 
 # Adding new columns with mathematical operations
 ?log10
-
+natData$log10TotalCases <- log10(natData$TotalCases)
+natData$casesPer1000 <- natData$TotalCases/(natData$Population/1000)
+head(natData)
 
 # Add new columns with string manipulation
-
+natData$region_cap <- tools::toTitleCase(as.character(natData$region))
+head(natData)
 
 # T Tests
 # Check regional difference
-
+natData[natData$Half == "West",]
+natData[natData$Half == "West", "casesPer1000"]
+group_west <- natData[natData$Half == "West", "casesPer1000"]
+group_east <- natData[natData$Half == "East", "casesPer1000"]
+t.test(group_west, group_east)
 
 # Check voting difference
-
+group_trump <- natData[natData$Vote == "Trump", "casesPer1000"]
+group_biden <- natData[natData$Vote == "Biden", "casesPer1000"]
+t.test(group_trump, group_biden)
 
 # Check NBA difference 
+group_noTeam <- natData[natData$NBA == "None", "casesPer1000"]
+group_hasTeam <- natData[natData$NBA == "West" | natData$NBA == "East", "casesPer1000"]
+t.test(group_noTeam, group_hasTeam)
 
+group_westTeam <- natData[natData$NBA == "West", "casesPer1000"]
+group_eastTeam <- natData[natData$NBA == "East", "casesPer1000"]
+t.test(group_westTeam, group_eastTeam)
 
 # Example Join
-
+test_merge <- merge(covidData, natData)
+View(head(test_merge))
 
 ### Plot data with ggplot2 and save a graph - Eric
 
 # Install ggplot2 package
-
+install.packages("ggplot2")
+library(ggplot2)
 
 ## plot a bar plot with ggplot2
+bar_plot <- ggplot(natData) +
+  aes(x = State, y = casesPer1000, fill = Half) +
+  geom_bar(stat = "identity")
+bar_plot
 
 
 # add a main title and axis titles
-
+bar_plot_cool <- bar_plot +
+  scale_fill_manual(
+    ""
+    , values = c("East" = "chartreuse3", "West" = "bisque4")
+  ) +
+  labs(
+    title = "COVID-19 East vs West Edition"
+    , y = "Cases per 1000"
+  )
+bar_plot_cool
 
 # but Eric, I don't like the aesthetic. So let's add some pzazz
 
@@ -92,6 +134,7 @@ multnomahCounty$Date <- as.POSIXct(multnomahCounty$Date)
 ## save a plot
 # I made a pretty picture, how do I share it on social media? (or put it in a paper. you do you.)
 ?ggsave
+ggsave("very_biqsue_eaast_vs_west.png", bar_plot_cool, dpi = 300, width = 9, units = "in")
 
 
 # your turn! can you save the bar plot we made?
